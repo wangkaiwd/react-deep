495a785d7ba18bf4d64a14df5db7f3b5952af6d2
wangkaiwd <1162486176@qq.com>
2019-11-16 01:02:16 +0800

    feat: auto import tsconfig paths config for jest config file

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,11 @@
+// jest.config.js
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+// In the following statement, replace `./tsconfig` with the path to your `tsconfig` file
+// which contains the path mapping (ie the `compilerOptions.paths` option):
+const { compilerOptions } = require('./tsconfig');
+
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
-  }
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths)
 };
