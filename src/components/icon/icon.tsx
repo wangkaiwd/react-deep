08a5bf3169cc2f1c7028ffb03b1ab17313ea07c6
wangkaiwd <1162486176@qq.com>
2019-11-21 00:18:12 +0800

    feat: display icon in browser successful

--- src/components/icon/icon.tsx
+++ src/components/icon/icon.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import wechat from '@/assets/svgs/wechat.svg';
+import '@/assets/svgs/wechat.svg';
 import './icon.scss';
 
 interface Props {
@@ -7,8 +7,8 @@ interface Props {
 }
 const Icon: React.FunctionComponent<Props> = (props) => {
   return (
-    <svg className="wui-icon" viewBox={`${wechat.viewBox}`}>
-      <use xlinkHref={`#${wechat}`}/>
+    <svg className="wui-icon">
+      <use xlinkHref={`#${props.name}`}/>
     </svg>
   );
 };
