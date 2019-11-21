badc5c4d7762e0461145b8517e697871c0989d02
wangkaiwd <1162486176@qq.com>
2019-11-21 23:44:52 +0800

    feat: use spread operator

--- src/components/icon/icon.tsx
+++ src/components/icon/icon.tsx
@@ -6,9 +6,10 @@ interface Props extends React.SVGAttributes<SVGElement> {
   name: string;
 }
 const Icon: React.FunctionComponent<Props> = (props) => {
+  const { name, ...rest } = props;
   return (
-    <svg className="wui-icon" onClick={props.onClick}>
-      <use xlinkHref={`#${props.name}`}/>
+    <svg className="wui-icon" {...rest}>
+      <use xlinkHref={`#${name}`}/>
     </svg>
   );
 };
