dc5f9a7b3dc7f6e6e018df46b28353b2c0931a05
wangkaiwd <1162486176@qq.com>
2019-11-21 23:56:33 +0800

    feat: encounter multiple className problem

--- src/components/icon/icon.tsx
+++ src/components/icon/icon.tsx
@@ -6,7 +6,7 @@ interface Props extends React.SVGAttributes<SVGElement> {
   name: string;
 }
 const Icon: React.FunctionComponent<Props> = (props) => {
-  const { name, ...rest } = props;
+  const { name, className, ...rest } = props;
   return (
     <svg className="wui-icon" {...rest}>
       <use xlinkHref={`#${name}`}/>
