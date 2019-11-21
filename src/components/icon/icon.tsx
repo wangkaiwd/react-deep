16018598be1fbb03ff47bdc206b3baecf2228f18
wangkaiwd <1162486176@qq.com>
2019-11-21 23:22:58 +0800

    feat: support onClick for icon component

--- src/components/icon/icon.tsx
+++ src/components/icon/icon.tsx
@@ -4,13 +4,17 @@ import './icon.scss';
 
 interface Props {
   name: string;
+  onClick?: React.MouseEventHandler<SVGElement>;
 }
 const Icon: React.FunctionComponent<Props> = (props) => {
   return (
-    <svg className="wui-icon">
+    <svg className="wui-icon" onClick={props.onClick}>
       <use xlinkHref={`#${props.name}`}/>
     </svg>
   );
 };
 
 export default Icon;
+Icon.defaultProps = {
+  onClick: () => {},
+};
--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,15 @@ import * as React from 'react';
 import Icon from '@/components/icon/icon';
 import { Fragment } from 'react';
 
+const fn: React.MouseEventHandler<SVGElement> = (e) => {
+  console.log('fn');
+  // e.target：事件触发的元素。可以用来实现事件委托(event delegation)
+  // e.currentTarget: 事件绑定的元素
+  console.log(e.target);
+};
 render(
   <Fragment>
-    <Icon name="wechat"/>
+    <Icon name="wechat" onClick={fn}/>
     <Icon name="alipay"/>
     <Icon name="github"/>
   </Fragment>, document.getElementById('root'));
