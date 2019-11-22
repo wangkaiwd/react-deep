79d5b6e8e9453ea792a66d3a05f332fa1b339df3
wangkaiwd <1162486176@qq.com>
2019-11-23 00:28:58 +0800

    feat: icon component click event test successfully

--- jest.config.js
+++ jest.config.js
@@ -3,10 +3,11 @@ module.exports = {
   preset: 'ts-jest',
   verbose: true,
   testMatch: ['**/__tests__/**/*.unit.{ts,tsx}'],
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
     '\\.(css|less|scss)$': '<rootDir>/__mocks__/styleMock.ts',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.ts',
   },
+  setupFilesAfterEnv: ['./test/setupTests.ts'],
 };
--- src/components/icon/__tests__/icon.unit.tsx
+++ src/components/icon/__tests__/icon.unit.tsx
@@ -1,11 +1,17 @@
 import renderer from 'react-test-renderer';
 import Icon from '@/components/icon/icon';
 import * as React from 'react';
-
+import { mount } from 'enzyme';
 // 测试引入scss文件的组件：@see:https://github.com/facebook/jest/issues/3094#issuecomment-284867098
 describe('icon 组件', () => {
-  it('xxx', () => {
+  it('渲染成功', () => {
     const json = renderer.create(<Icon name="alipay"/>);
     expect(json).toMatchSnapshot();
+  });
+  it('支持点击事件', () => {
+    const fn = jest.fn();
+    const component = mount(<Icon name="alipay" onClick={fn}/>);
+    component.find('svg').simulate('click');
+    expect(fn).toBeCalled();
   });
 });
