c50812105e9beb4b14eac623d848c9821e2f7d77
wangkaiwd <1162486176@qq.com>
2019-11-22 23:42:37 +0800

    feat: update icon test snapshots

--- src/components/icon/__tests__/icon.unit.tsx
+++ src/components/icon/__tests__/icon.unit.tsx
@@ -5,7 +5,7 @@ import * as React from 'react';
 // 测试引入scss文件的组件：@see:https://github.com/facebook/jest/issues/3094#issuecomment-284867098
 describe('icon 组件', () => {
   it('xxx', () => {
-    const json = renderer.create(<Icon name="qq"/>);
+    const json = renderer.create(<Icon name="alipay"/>);
     expect(json).toMatchSnapshot();
   });
 });
