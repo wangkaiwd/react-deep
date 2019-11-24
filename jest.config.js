23c6566a3f38929df4876a658b1ccef38a02eb98
wangkaiwd <1162486176@qq.com>
2019-11-24 20:41:12 +0800

    feat: add test reporters jest-junit

--- jest.config.js
+++ jest.config.js
@@ -11,6 +11,6 @@ module.exports = {
   },
   setupFilesAfterEnv: ['./test/setupTests.ts'],
   collectCoverage: true,
-  reporters: ['default'],
+  reporters: ['default', 'jest-junit'],
   collectCoverageFrom: ['src/**/*.{ts,tsx}', '!**/node_modules/**'],
 };
