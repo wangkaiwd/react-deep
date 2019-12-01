fd657f6a34bdf752de733ab2544a5d75b4043477
wangkaiwd <1162486176@qq.com>
2019-12-01 18:06:45 +0800

    feat: use portals to modal content

--- src/components/dialog/dialog.tsx
+++ src/components/dialog/dialog.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './dialog.scss';
 import { Icon } from '../../index';
+import { createPortal } from 'react-dom';
 
 interface Props {
   visible: boolean;
 }
 const Dialog: React.FC<Props> = (props) => {
-  return props.visible ? (
+  const modal = (
     <div className="wui-dialog">
       <div className="wui-dialog-mask"/>
       <div className="wui-dialog-content">
@@ -16,7 +17,8 @@ const Dialog: React.FC<Props> = (props) => {
         </div>
       </div>
     </div>
-  ) : null;
+  );
+  return props.visible ? createPortal(modal, document.body) : null;
 };
 
 export default Dialog;
