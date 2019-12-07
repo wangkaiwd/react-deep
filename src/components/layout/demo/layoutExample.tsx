2f32e1f99c27d6f6f639fddc498e6286c6c167a1
wangkaiwd <1162486176@qq.com>
2019-12-07 23:56:42 +0800

    feat: implement fourth example

--- src/components/layout/demo/layoutExample.tsx
+++ src/components/layout/demo/layoutExample.tsx
@@ -32,6 +32,15 @@ const LayoutExample = () => {
         </Layout>
         <Footer>Footer</Footer>
       </Layout>
+      <h3 style={{ marginTop: '12px' }}>例子4</h3>
+      <Layout style={{ height: '600px', border: '1px solid red' }}>
+        <Aside>Aside</Aside>
+        <Layout>
+          <Header>Header</Header>
+          <Content>Content</Content>
+          <Footer>Footer</Footer>
+        </Layout>
+      </Layout>
     </Fragment>
   );
 };
