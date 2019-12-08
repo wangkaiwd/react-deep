6cbba28e8e0bfd2041312b51bb7fec464b385c9f
wangkaiwd <1162486176@qq.com>
2019-12-09 00:39:45 +0800

    feat: optimize classes and fixedPrefixClasses

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,25 +1,33 @@
-export const classes = (...names: (string | undefined | false | null)[]): string => {
-  return names.filter(Boolean).join(' ');
-};
 interface XXX {
   [k: string]: string | boolean | null | undefined;
 }
+type ClassNames = (string | undefined | false | null | XXX)[];
+
+export const classes = (...names: ClassNames): string => {
+  return makeClasses(names);
+};
+
 export const fixedPrefixClasses = (prefix: string) => {
-  return (...names: (string | undefined | false | null | XXX)[]) => {
+  return (...names: ClassNames) => {
     if (names.length === 0) {return prefix;}
-    return names.filter(Boolean).reduce((result: string[], name) => {
+    return makeClasses(names, prefix);
+  };
+};
+const makeClasses = (names: ClassNames, prefix: string = ''): string => {
+  return (names.filter(Boolean) as (string | XXX)[])
+    .reduce((result: (string | XXX)[], name) => {
       if (isPlainObject(name)) {
         const keys = Object.keys(name);
         for (let i = 0; i < keys.length; i++) {
           const key = keys[i];
-          name[key] && result.push(`${prefix}-${key}`);
+          name[key] && result.push(prefix ? `${prefix}-${key}` : key);
           return result;
         }
       }
-      result.push(`${prefix}-${name}`);
+      result.push(prefix ? `${prefix}-${name}` : name);
       return result;
-    }, []).join(' ');
-  };
+    }, [])
+    .join(' ');
 };
 
 const isPlainObject = (value: any): value is object => {
