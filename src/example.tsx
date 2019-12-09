0d1157329a9847f48bbcebd0eb4884cacb71d4fc
wangkaiwd <1162486176@qq.com>
2019-12-10 00:59:24 +0800

    feat: min-height make content scrollable

--- src/example.tsx
+++ src/example.tsx
@@ -1,37 +1,41 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
-import {
-  HashRouter as Router,
-  Switch,
-  Route,
-  Link,
-} from 'react-router-dom';
+import { HashRouter as Router, Link, Route, Switch, Redirect } from 'react-router-dom';
 import menus from './contant/menus';
 import './assets/styles/base.scss';
 import './assets/styles/reset.scss';
 import './example.scss';
+import { Aside, Content, Footer, Header, Layout } from './components/layout/layout';
 
+const $ = (selector: string) => document.querySelector<HTMLElement>(selector);
 const App = () => {
   return (
     <Router>
-      <div className="example">
-        <ul className="menu">
-          {menus.map((menu) => (
-            <li key={menu.path}>
-              <Link to={menu.path}>{menu.name}</Link>
-            </li>
-          ))}
-        </ul>
-        <div className="component-wrapper">
-          <Switch>
-            {menus.map((menu) => (
-              <Route path={menu.path} key={menu.path}>
-                <menu.component/>
-              </Route>
-            ))}
-          </Switch>
-        </div>
-      </div>
+      <Layout className="example">
+        <Header className="header">Header</Header>
+        <Layout style={{ border: '1px solid red' }} className="content">
+          <Aside style={{ border: '1px solid green' }}>
+            <ul className="menu">
+              {menus.map((menu) => (
+                <li key={menu.path}>
+                  <Link to={menu.path}>{menu.name}</Link>
+                </li>
+              ))}
+            </ul>
+          </Aside>
+          <Content className="component-wrapper">
+            <Switch>
+              {menus.map((menu) => (
+                <Route path={menu.path} key={menu.path}>
+                  <menu.component/>
+                </Route>
+              ))}
+              <Redirect to={'/icon'}/>
+            </Switch>
+          </Content>
+        </Layout>
+        <Footer className="footer">Footer</Footer>
+      </Layout>
     </Router>
   );
 };
