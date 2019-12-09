e394c30eb9482a3e2035a9a2958ba9d9aa45a1e0
wangkaiwd <1162486176@qq.com>
2019-12-09 23:47:36 +0800

    feat: optimize module export

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -25,3 +25,8 @@ const Layout: React.FC<Props> = (props) => {
 };
 
 export default Layout;
+export { Layout };
+export { default as Header } from './header';
+export { default as Content } from './content';
+export { default as Aside } from './aside';
+export { default as Footer } from './footer';
