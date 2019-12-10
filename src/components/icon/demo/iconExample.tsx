d2490d98794f71a3cc31c949338a9868e20c435c
wangkaiwd <1162486176@qq.com>
2019-12-11 00:26:59 +0800

    feat: implement source code demonstrate style

--- src/components/icon/demo/iconExample.tsx
+++ src/components/icon/demo/iconExample.tsx
@@ -1,14 +1,17 @@
 import React, { Fragment } from 'react';
 import Icon from '../icon';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { darcula } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import './iconExample.scss';
 
 const code = require('!!raw-loader!./iconExample.tsx');
 const IconExample = () => {
   return (
     <Fragment>
       <Icon name="alipay"/>
-      <pre>
+      <SyntaxHighlighter className={'highlight-code'} language="tsx" style={darcula}>
         {code.default}
-      </pre>
+      </SyntaxHighlighter>
     </Fragment>
   );
 };
