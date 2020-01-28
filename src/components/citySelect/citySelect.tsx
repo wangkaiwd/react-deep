0e521b6126fbd4414a71fda4a1687b05b9656c6c
wangkaiwd <1162486176@qq.com>
2020-01-28 13:10:12 +0800

    feat: complete letter style

--- src/components/citySelect/citySelect.tsx
+++ src/components/citySelect/citySelect.tsx
@@ -1,5 +1,9 @@
 import React, { FC, HTMLAttributes } from 'react';
 import { classes, fixedPrefixClasses } from '../../utils/helpers';
+import './citySelect.scss';
+
+const LETTERS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K',
+  'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'W', 'X', 'Y', 'Z'];
 
 interface IProps extends HTMLAttributes<HTMLDivElement> {
   currentCity: string;
@@ -10,7 +14,21 @@ const sc = classes;
 const CitySelect: FC<IProps> = (props) => {
   return (
     <div className={sc(fixSc(), props.className)}>
-      {props.currentCity}
+      <header className={'header'}>
+        选择城市
+      </header>
+      <div className={fixSc('allCity')}>
+        <div className={fixSc('letters')}>
+          {
+            LETTERS.map((letter) => (
+              <div className={fixSc('letter')} key={letter}>
+                {letter}
+              </div>
+            ))
+          }
+        </div>
+        <div className={fixSc('cities')}></div>
+      </div>
     </div>
   );
 };
