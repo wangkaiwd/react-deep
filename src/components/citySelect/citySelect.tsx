90187e606596af0c02db32c9c5e87af29aaaced1
wangkaiwd <1162486176@qq.com>
2020-01-28 18:32:21 +0800

    feat: get location by free api

--- src/components/citySelect/citySelect.tsx
+++ src/components/citySelect/citySelect.tsx
@@ -1,22 +1,41 @@
-import React, { FC, HTMLAttributes } from 'react';
+import React, { FC, HTMLAttributes, useEffect, useState } from 'react';
 import { classes, fixedPrefixClasses } from '../../utils/helpers';
 import './citySelect.scss';
+import Icon from '../icon/icon';
 
 const LETTERS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K',
   'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'W', 'X', 'Y', 'Z'];
 
 interface IProps extends HTMLAttributes<HTMLDivElement> {
-  currentCity: string;
 }
 
 const fixSc = fixedPrefixClasses('wui-city-select');
 const sc = classes;
 const CitySelect: FC<IProps> = (props) => {
+  const [location, setLocation] = useState('没有开启定位');
+  useEffect(() => {
+    const getCurrentCity = () => {
+      const xhr = new XMLHttpRequest();
+      xhr.open('GET', 'https://api.qzone.work/api/ip.address');
+      xhr.send();
+      xhr.addEventListener('readystatechange', (e) => {
+        const request = e.target as XMLHttpRequest;
+        if (request.readyState === 4 && request.status === 200) {
+          const response = JSON.parse(request.responseText);
+          setLocation(response.data.city);
+        }
+      });
+    };
+    getCurrentCity();
+  }, []);
   return (
     <div className={sc(fixSc(), props.className)}>
-      <header className={'header'}>
+      <header className={fixSc('header')}>
         选择城市
       </header>
+      <div className={fixSc('location')}>
+        定位城市: <Icon className={fixSc('icon-location')} name="location"/>{location}
+      </div>
       <div className={fixSc('allCity')}>
         <div className={fixSc('letters')}>
           {
--- src/components/citySelect/demo/citySelectExample.tsx
+++ src/components/citySelect/demo/citySelectExample.tsx
@@ -3,15 +3,13 @@ import CitySelect from '../citySelect';
 import Button from '../../button/button';
 
 const CitySelectExample = () => {
-  const [currentCity, setCurrentCity] = useState('暂未选择');
   const [visible, setVisible] = useState(false);
   return (
     <div>
-      <div><span>当前城市:</span><span>{currentCity}</span></div>
       <Button level={'primary'} onClick={() => setVisible(true)}>选择城市</Button>
       {
         visible &&
-        <CitySelect currentCity={currentCity}/>
+        <CitySelect/>
       }
     </div>
   );
